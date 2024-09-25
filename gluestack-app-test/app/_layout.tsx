1648cfc1a06fe7d5f0699ab9ff721403b9c2afc6
Marcos Willian <markusuuuu@gmail.com>
2024-09-24 23:45:29 -0300

    chore: testing gluestack v2 on expo and nextjs

--- gluestack-app-test/app/_layout.tsx
+++ gluestack-app-test/app/_layout.tsx
@@ -1,9 +1,43 @@
+import "react-native-reanimated";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { Stack } from "expo-router";
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <GluestackUIProvider mode="light">
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="index" />
+        </Stack>
+      </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
--- gluestack-app-test/app/index.tsx
+++ gluestack-app-test/app/index.tsx
@@ -5,43 +5,55 @@ import {
   ActionsheetContent,
   ActionsheetDragIndicator,
   ActionsheetDragIndicatorWrapper,
+  ActionsheetFlatList,
   ActionsheetItem,
   ActionsheetItemText,
 } from "@/components/ui/actionsheet";
-import { Button } from "react-native";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function Index() {
   const [showActionsheet, setShowActionsheet] = React.useState(false);
-
   const handleClose = () => setShowActionsheet(false);
+  const DATA = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "First Item",
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Second Item",
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: "Third Item",
+    },
+  ];
+
+  const Item = React.useCallback(
+    ({ title }: { title: string }) => (
+      <ActionsheetItem onPress={handleClose}>
+        <ActionsheetItemText>{title}</ActionsheetItemText>
+      </ActionsheetItem>
+    ),
+    [handleClose]
+  );
 
   return (
     <>
-      <Button
-        onPress={() => setShowActionsheet(true)}
-        title="Open Actionsheet"
-      />
-      <Actionsheet isOpen onClose={handleClose}>
+      <Button onPress={() => setShowActionsheet(true)} variant="solid">
+        <ButtonText>Open</ButtonText>
+      </Button>
+      <Actionsheet isOpen={showActionsheet} onClose={handleClose}>
         <ActionsheetBackdrop />
         <ActionsheetContent>
           <ActionsheetDragIndicatorWrapper>
             <ActionsheetDragIndicator />
           </ActionsheetDragIndicatorWrapper>
-          <ActionsheetItem onPress={handleClose}>
-            <ActionsheetItemText>Edit Message</ActionsheetItemText>
-          </ActionsheetItem>
-          <ActionsheetItem onPress={handleClose}>
-            <ActionsheetItemText>Mark Unread</ActionsheetItemText>
-          </ActionsheetItem>
-          <ActionsheetItem onPress={handleClose}>
-            <ActionsheetItemText>Remind Me</ActionsheetItemText>
-          </ActionsheetItem>
-          <ActionsheetItem onPress={handleClose}>
-            <ActionsheetItemText>Add to Saved Items</ActionsheetItemText>
-          </ActionsheetItem>
-          <ActionsheetItem isDisabled onPress={handleClose}>
-            <ActionsheetItemText>Delete</ActionsheetItemText>
-          </ActionsheetItem>
+          <ActionsheetFlatList
+            data={DATA}
+            renderItem={({ item }) => <Item title={item.title} />}
+            keyExtractor={(item) => item.id}
+          />
         </ActionsheetContent>
       </Actionsheet>
     </>
